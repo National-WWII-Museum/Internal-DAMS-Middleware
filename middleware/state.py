a30140ae0bbbb0a348763ccb427413417ddc140a
Ben B <benjamin.beard@nationalww2museum.org>
2026-07-27 13:13:57 -0700

    Auto stash before merge of "bb-EmuGet" and "origin/bb-EmuGet"

--- middleware/state.py
+++ middleware/state.py
@@ -50,7 +50,7 @@ def init_db():
     finally:
         conn.close()
 
-def get_last_sync_date(module, default="2026-01-01"):
+def get_last_sync_date(module, default="2026-03-01"):
     """Returns the date to use as the next 'gte' filter for this module."""
     conn = get_connection()
     try:
